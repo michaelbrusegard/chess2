eee8087a4c49260b084b7dff484af6650f497bdc
oddfjell <ann.marie.amr@gmail.com>
2025-03-03 02:44:32 +0100

    In progress game screen states, not done

--- chesstwo_model/src/main/kotlin/io/github/chess2/ExampleFile.kt
+++ chesstwo_model/src/main/kotlin/io/github/chess2/ExampleFile.kt
@@ -1,5 +1,5 @@
 package io.github.chess2
 
 class ExampleFile {
-    val randomText: String = "this is some random text"
+    val randomText: String = "this is some random text2"
 }
--- chesstwo_presenter/src/main/kotlin/io/github/chess2/Chess2Game.kt
+++ chesstwo_presenter/src/main/kotlin/io/github/chess2/Chess2Game.kt
@@ -1,8 +1,21 @@
 package io.github.chess2
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputMultiplexer
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
+import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
+import io.github.chess2.View.GameScreen
+import io.github.chess2.View.LobbyScreen
+import io.github.chess2.View.MenuScreen
+import io.github.chess2.View.PauseScreen
+import io.github.chess2.View.SettingsScreen
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
@@ -10,14 +23,59 @@ import ktx.assets.disposeSafely
 import ktx.assets.toInternalFile
 import ktx.async.KtxAsync
 import ktx.graphics.use
-import io.github.chess2.ExampleFile
+import io.github.chess2.View.SplashScreen
+import ktx.scene2d.Scene2DSkin
+import ktx.style.skin
 
 class Chess2Game : KtxGame<KtxScreen>() {
+    lateinit var skin: Skin
+    lateinit var font: BitmapFont
+
     override fun create() {
         KtxAsync.initiate()
 
-        addScreen(FirstScreen())
-        setScreen<FirstScreen>()
+        val inputMultiplexer = InputMultiplexer()
+        Gdx.input.inputProcessor = inputMultiplexer
+
+        makeButton();
+
+        //addScreen(FirstScreen())
+        //setScreen<FirstScreen>()
+
+        addScreen(MenuScreen(this))
+        addScreen(LobbyScreen(this))
+        addScreen(GameScreen(this))
+        addScreen(PauseScreen(this))
+        addScreen(SettingsScreen(this))
+        addScreen(SplashScreen(this))
+
+
+        setScreen<SplashScreen>()
+    }
+
+    override fun dispose() {
+        super.dispose()
+        skin.dispose()
+        font.dispose()
+    }
+
+    fun makeButton(){
+        skin = Skin()
+        font = BitmapFont()
+        skin.add("default", font)
+
+        val pixmap = Pixmap(1, 1, Pixmap.Format.RGBA8888)
+        pixmap.setColor(Color.WHITE)
+        pixmap.fill()
+        skin.add("white", Texture(pixmap))
+
+        val buttonStyle = TextButton.TextButtonStyle()
+        buttonStyle.up = TextureRegionDrawable(skin.getRegion("white"))
+        buttonStyle.font = font
+        buttonStyle.fontColor = Color.BLACK
+        skin.add("default", buttonStyle)
+
+        Scene2DSkin.defaultSkin = skin
     }
 }
 
